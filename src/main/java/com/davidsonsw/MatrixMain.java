eea8569d7a377963600490d42082a8824a26c24c
Scott Davidson <scottwdavidson@users.noreply.github.com>
2023-12-08 17:24:42 -0800

    Added DMatrixRMaj

--- src/main/java/com/davidsonsw/MatrixMain.java
+++ src/main/java/com/davidsonsw/MatrixMain.java
@@ -1,6 +1,10 @@
 package com.davidsonsw;
 
 import org.ejml.simple.SimpleMatrix;
+import org.ejml.data.DMatrixRMaj;
+import org.ejml.dense.row.CommonOps_DDRM;
+import org.ejml.data.DMatrixIterator;
+
 
 public class MatrixMain {
 
@@ -47,7 +51,8 @@ public class MatrixMain {
     public static void main(String[] args) {
 //        simpleActivation();
 //        multipleActivation();
-        plusExample();
+//        plusExample();
+        playWithDMatrix();
     }
 
     private static void simpleActivation() {
@@ -88,4 +93,52 @@ public class MatrixMain {
         System.out.println(result);
     }
 
+    private static void playWithDMatrix() {
+        DMatrixRMaj matrix1 = new DMatrixRMaj(new double[][]{{1, 2}, {3, 4}});
+        DMatrixRMaj matrix2 = new DMatrixRMaj(new double[][]{{5, 6}, {7, 8}});
+
+        // Create an empty matrix to store the result
+        DMatrixRMaj result = new DMatrixRMaj(matrix1.numRows, matrix2.numCols);
+
+        // Multiply the matrices
+        CommonOps_DDRM.mult(matrix1, matrix2, result);
+
+        // Print the result
+        for (int i = 0; i < result.numRows; i++) {
+            for (int j = 0; j < result.numCols; j++) {
+                System.out.print(result.get(i, j) + " ");
+            }
+            System.out.println();
+        }
+
+        // Add the matrices
+        CommonOps_DDRM.add(matrix1, matrix2, result);
+
+        // Print the result
+        for (int i = 0; i < result.numRows; i++) {
+            for (int j = 0; j < result.numCols; j++) {
+                System.out.print(result.get(i, j) + " ");
+            }
+            System.out.println();
+        }
+
+        DMatrixIterator iterator = result.iterator(true, 0, 0, result.numRows-1, result.numCols-1);
+
+        // Iterate through the matrix
+        while (iterator.hasNext()) {
+            double value = iterator.next();
+            int index = iterator.getIndex();
+            System.out.println("(" + index + "): " + value);
+            result.set(index, value + 3.5);
+        }
+        iterator = result.iterator(true, 0, 0, result.numRows-1, result.numCols-1);
+
+        // Iterate through the matrix
+        while (iterator.hasNext()) {
+            double value = iterator.next();
+            int index = iterator.getIndex();
+            System.out.println("(" + index + "): " + value);
+        }
+
+    }
 }
